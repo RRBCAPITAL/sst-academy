74969aadacc3a9b8dff1ef802bc6c172c66093bf
RRBCAPITAL <RRBCAPITAL24@gmail.com>
2024-08-27 21:07:44 -0500

    started the proyect 1.2

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,12 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextResponse } from 'next/server'
  
-type ResponseData = {
-  message: string
-}
- 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
-) {
-  res.status(200).json({ message: 'Hello from Next.js!' })
+export async function GET(request: Request) {
+  return NextResponse.json({ msg: 'Hello from server' })
 }
